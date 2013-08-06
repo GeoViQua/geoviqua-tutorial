68b24be9023629df321d9bbb9f9213f548c71954
Chris Lush <chris.lush@gmail.com>
2013-08-06 21:40:32 +0100

    Generate a new UUID for the XML document to be published if a duplicate key already exists on the target GeoNetwork instance.

--- publish.php
+++ publish.php
@@ -2,6 +2,9 @@
 
 session_start();
 
+// error tracking
+$error = array();
+
 // name and path of the configuration file for this script
 $config_file = dirname(__FILE__) . "/app/config/config.ini";
 
@@ -9,159 +12,181 @@ $config_file = dirname(__FILE__) . "/app/config/config.ini";
 if (file_exists($config_file) && is_readable($config_file)) {
 
 	$config = parse_ini_file($config_file);
+
+    // metadata document to be published
+    $gvq_doc = new DOMDocument();
+
+    if ($_FILES["publish-metadata"]["size"] > 0 && $_FILES["publish-metadata"]["error"] === UPLOAD_ERR_OK) {
+
+        // read the contents of the uploaded file
+        $xml = file_get_contents($_FILES["publish-metadata"]["tmp_name"]);
+
+        // simple check to see if it's a well-formed XML document, suppressing warnings
+        $previous_errors = libxml_use_internal_errors(true);
+        $valid = $gvq_doc->loadXML($xml);
+        libxml_clear_errors();
+        libxml_use_internal_errors($previous_errors);
+
+        if ($valid === false) {
+
+            $error = array(
+                "status" => "error",
+                "message" => "The XML document could not be parsed, please check that the XML is well-formed."
+            );
+        }
+        else {
+
+            // begin the upload process
+            if (authenticateWithGeoNetwork($config["editor_username"], $config["editor_password"])) {
+
+                // track the number of iterations to prevent an infinite loop
+                $upload_attempts = 0;
+
+                // get the original UUID
+                $xpath = new DOMXPath($gvq_doc);
+                $uuid_query = $xpath->query('//gmd:fileIdentifier/gco:CharacterString');
+                $original_uuid = $uuid_query->item(0)->nodeValue;
+
+                do {
+
+                    // try to upload the document
+                    $response = uploadToGeoNetwork($gvq_doc->saveXML());
+                    $error = errorCheck($response["body"]);
+                    $uuid_violation = strpos($error['message'], 'Unique index or primary key violation: "CONSTRAINT_INDEX_1 ON PUBLIC.METADATA(UUID)";');
+
+                    if ($uuid_violation !== false) {
+
+                        // there was a UUID violation so we have to generate a new one
+                        // http://stackoverflow.com/a/2040279/1983684
+                        mt_srand(crc32(serialize(microtime(true))));
+                        $new_uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                            // 32 bits for "time_low"
+                            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+                            // 16 bits for "time_mid"
+                            mt_rand(0, 0xffff),
+
+                            // 16 bits for "time_hi_and_version",
+                            // four most significant bits holds version number 4
+                            mt_rand(0, 0x0fff) | 0x4000,
+
+                            // 16 bits, 8 bits for "clk_seq_hi_res",
+                            // 8 bits for "clk_seq_low",
+                            // two most significant bits holds zero and one for variant DCE1.1
+                            mt_rand(0, 0x3fff) | 0x8000,
+
+                            // 48 bits for "node"
+                            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+                        );
+
+                        // update the UUID for the next upload attempt
+                        $uuid_query->item(0)->nodeValue = $new_uuid;
+                    }
+
+                    $upload_attempts++;
+                }
+                while (($uuid_violation !== false) && ($upload_attempts < 5));
+
+                if (empty($error) &&
+                    authenticateWithGeoNetwork($config["admin_username"], $config["admin_password"])) {
+
+                    // parse the reponse for the metadata's catalog id and uuid
+                    $parsed = new SimpleXMLElement($response["body"]);
+
+                    // create a privileges request
+                    $xml = new DOMDocument("1.0");
+                    $xml->formatOutput = true;
+                    $root_node = $xml->createElement("request");
+                    $xml->appendChild($root_node);
+                    // id element
+                    $id_node = $xml->createElement("id");
+                    $id_node_value = $xml->createTextNode($parsed->id);
+                    $id_node->appendChild($id_node_value);
+                    // append privilege elements to root
+                    $root_node->appendChild($xml->createElement("_1_0")); // group 1 (all) can view
+                    $root_node->appendChild($xml->createElement("_1_1")); // group 1 (all) can download
+                    $root_node->appendChild($xml->createElement("_" . $config["editor_group_id"] . "_2")); // editor's user group can edit
+                    // append all other child elements to root
+                    $root_node->appendChild($id_node);
+                    // save XML as string
+                    $xml_request = $xml->saveXML();
+
+                    // send the request to GeoNetwork
+                    $response = sendXMLRequest("xml.metadata.privileges", $xml_request);
+                    $error = errorCheck($response["body"]);
+
+                    if (empty($error)) {
+
+                        // parse the reponse for the metadata's catalog id
+                        $parsed = new SimpleXMLElement($response["body"]);
+
+                        // job done, send a success response
+                        $json = array(
+                            "status" => "success",
+                            "data"  => array(
+                                "geonetwork" => array(
+                                    "username" => $config["editor_username"],
+                                    "password" => $config["editor_password"],
+                                    "url" => $config["geonetwork_baseURL"],
+                                    "metadata_id" => (int) $parsed->id
+                                )
+                            )
+                        );
+
+                        die(json_encode($json));
+                    }
+                }
+            }
+        }
+    }
+    else {
+
+        $path_parts = pathinfo($_FILES['publish-metadata']['name']);
+        $filename = $path_parts['filename'];
+        $message = "";
+
+        // something went wrong
+        switch($_FILES["publish-metadata"]["error"]) {
+
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                $message = "$filename must be smaller than " . ini_get("upload_max_filesize") . " in size.";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $message = "$filename was only partially uploaded.";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $message = "No file was selected for upload.";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+            case UPLOAD_ERR_CANT_WRITE:
+            case UPLOAD_ERR_EXTENSION:
+                $message = "Unable to write $filename to disk.";
+                break;
+            default:
+                $message = "Please select your transformed metadata document.";
+                break;
+        }
+
+        $error = array(
+            "status" => "error",
+            "message" => $message
+        );
+    }
 }
 else {
 
-    $json = array(
+    $error = array(
         "status" => "error",
         "message" => "Could not load configuration file $config_file"
     );
-
-    die(json_encode($json));
 }
 
-// metadata document to be published
-$gvq_doc = "";
+// execution was not successful
+if (!empty($error)) {
 
-if ($_FILES["publish-metadata"]["size"] > 0 && $_FILES["publish-metadata"]["error"] === UPLOAD_ERR_OK) {
-
-    // read the contents of the uploaded file
-    $gvq_doc = file_get_contents($_FILES["publish-metadata"]["tmp_name"]);
-
-    // simple check to see if it's a well-formed XML document, suppressing warnings
-    libxml_use_internal_errors(true);
-    $valid = simplexml_load_string($gvq_doc);
-    libxml_clear_errors();
-
-    if ($valid === false) {
-
-        $json = array(
-            "status" => "error",
-            "message" => "The XML document could not be parsed, please check that the XML is well-formed."
-        );
-
-        die(json_encode($json));
-    }
-}
-else {
-
-    $path_parts = pathinfo($_FILES['publish-metadata']['name']);
-    $filename = $path_parts['filename'];
-    $message = "";
-
-    // something went wrong
-    switch($_FILES["publish-metadata"]["error"]) {
-
-        case UPLOAD_ERR_INI_SIZE:
-        case UPLOAD_ERR_FORM_SIZE:
-            $message = "$filename must be smaller than " . ini_get("upload_max_filesize") . " in size.";
-            break;
-        case UPLOAD_ERR_PARTIAL:
-            $message = "$filename was only partially uploaded.";
-            break;
-        case UPLOAD_ERR_NO_FILE:
-            $message = "No file was selected for upload.";
-            break;
-        case UPLOAD_ERR_NO_TMP_DIR:
-        case UPLOAD_ERR_CANT_WRITE:
-        case UPLOAD_ERR_EXTENSION:
-            $message = "Unable to write $filename to disk.";
-            break;
-        default:
-            $message = "Please select your transformed metadata document.";
-            break;
-    }
-
-    $json = array(
-        "status" => "error",
-        "message" => $message
-    );
-
-    die(json_encode($json));
-}
-
-if (authenticateWithGeoNetwork($config["editor_username"], $config["editor_password"])) {
-
-    // create a metadata insert request
-    $xml = new DOMDocument("1.0");
-    $xml->formatOutput = true;
-    $root_node = $xml->createElement("request");
-    $xml->appendChild($root_node);
-    // group element
-    $group_node = $xml->createElement("group");
-    $group_node_value = $xml->createTextNode("1"); // all
-    $group_node->appendChild($group_node_value);
-    // category element
-    $category_node = $xml->createElement("category");
-    $category_node_value = $xml->createTextNode("_none_");
-    $category_node->appendChild($category_node_value);
-    // stylesheet element
-    $stylesheet_node = $xml->createElement("stylesheet");
-    $stylesheet_node_value = $xml->createTextNode("_none_");
-    $stylesheet_node->appendChild($stylesheet_node_value);
-    // data element
-    $data_node = $xml->createElement("data");
-    $data_node_value = $xml->createCDATASection($gvq_doc); // the uploaded document embedded as CDATA
-    $data_node->appendChild($data_node_value);
-    // append child elements to root
-    $root_node->appendChild($group_node);
-    $root_node->appendChild($category_node);
-    $root_node->appendChild($stylesheet_node);
-    $root_node->appendChild($data_node);
-    // save XML as string
-    $xml_request = $xml->saveXML();
-
-    // send the request to GeoNetwork
-    $response = sendXMLRequest("xml.metadata.insert", $xml_request);
-
-    if (!errorCheck($response["body"]) &&
-        authenticateWithGeoNetwork($config["admin_username"], $config["admin_password"])) {
-
-        // parse the reponse for the metadata's catalog id and uuid
-        $parsed = new SimpleXMLElement($response["body"]);
-
-        // create a privileges request
-        $xml = new DOMDocument("1.0");
-        $xml->formatOutput = true;
-        $root_node = $xml->createElement("request");
-        $xml->appendChild($root_node);
-        // id element
-        $id_node = $xml->createElement("id");
-        $id_node_value = $xml->createTextNode($parsed->id);
-        $id_node->appendChild($id_node_value);
-        // append privilege elements to root
-        $root_node->appendChild($xml->createElement("_1_0")); // group 1 (all) can view
-        $root_node->appendChild($xml->createElement("_1_1")); // group 1 (all) can download
-        $root_node->appendChild($xml->createElement("_" . $config["editor_group_id"] . "_2")); // editor's user group can edit
-        // append all other child elements to root
-        $root_node->appendChild($id_node);
-        // save XML as string
-        $xml_request = $xml->saveXML();
-
-        // send the request to GeoNetwork
-        $response = sendXMLRequest("xml.metadata.privileges", $xml_request);
-
-        if (!errorCheck($response["body"])) {
-
-            // parse the reponse for the metadata's catalog id
-            $parsed = new SimpleXMLElement($response["body"]);
-
-            // job done, send a success response
-            $json = array(
-                "status" => "success",
-                "data"  => array(
-                    "geonetwork" => array(
-                        "username" => $config["editor_username"],
-                        "password" => $config["editor_password"],
-                        "url" => $config["geonetwork_baseURL"],
-                        "metadata_id" => (int) $parsed->id
-                    )
-                )
-            );
-
-            die(json_encode($json));
-        }
-    }
+    header("HTTP/1.1 500 Internal Server Error");
+    die(json_encode($error));
 }
 
 /**
@@ -173,7 +198,7 @@ if (authenticateWithGeoNetwork($config["editor_username"], $config["editor_passw
 function authenticateWithGeoNetwork($username, $password) {
 
     // :(
-    global $config;
+    global $config, $error;
 
     if (isset($_SESSION["auth_cookie"])) {
 
@@ -185,10 +210,15 @@ function authenticateWithGeoNetwork($username, $password) {
         $xml_request = $xml->saveXML();
 
         $response = sendXMLRequest("xml.user.logout", $xml_request);
+        $error = errorCheck($response["body"]);
 
-        if (!errorCheck($response["body"])) {
+        if (empty($error)) {
 
             unset($_SESSION["auth_cookie"]);
+        }
+        else {
+
+            return false;
         }
     }
 
@@ -213,16 +243,60 @@ function authenticateWithGeoNetwork($username, $password) {
 
     // send the request to GeoNetwork
     $response = sendXMLRequest("xml.user.login", $xml_request);
+    $error = errorCheck($response["body"]);
 
-    if (!errorCheck($response["body"])) {
+    if (empty($error)) {
 
         // we're authenticated, keep for future requests
         $_SESSION["auth_cookie"] = $response["header"]["Set-Cookie"];
+    }
+    else {
+
+        return false;
     }
 
     return true;
 }
 
+/**
+* Helper method to upload an XML document to GeoNetwork.
+*/
+function uploadToGeoNetwork($xml_doc) {
+
+    // create a metadata insert request
+    $xml = new DOMDocument("1.0");
+    $xml->formatOutput = true;
+    $root_node = $xml->createElement("request");
+    $xml->appendChild($root_node);
+    // group element
+    $group_node = $xml->createElement("group");
+    $group_node_value = $xml->createTextNode("1"); // all
+    $group_node->appendChild($group_node_value);
+    // category element
+    $category_node = $xml->createElement("category");
+    $category_node_value = $xml->createTextNode("_none_");
+    $category_node->appendChild($category_node_value);
+    // stylesheet element
+    $stylesheet_node = $xml->createElement("stylesheet");
+    $stylesheet_node_value = $xml->createTextNode("_none_");
+    $stylesheet_node->appendChild($stylesheet_node_value);
+    // data element
+    $data_node = $xml->createElement("data");
+    $data_node_value = $xml->createCDATASection($xml_doc); // the uploaded document embedded as CDATA
+    $data_node->appendChild($data_node_value);
+    // append child elements to root
+    $root_node->appendChild($group_node);
+    $root_node->appendChild($category_node);
+    $root_node->appendChild($stylesheet_node);
+    $root_node->appendChild($data_node);
+    // save XML as string
+    $xml_request = $xml->saveXML();
+
+    // send the request to GeoNetwork
+    $response = sendXMLRequest("xml.metadata.insert", $xml_request);
+
+    return $response;
+}
 
 /**
 * Helper method to send an XML request to GeoNetwork. The base URL is defined
@@ -302,13 +376,14 @@ function errorCheck($response) {
     $xml = new DOMDocument("1.0");
     $xml->loadXML($response);
     $errors = $xml->getElementsByTagName("error");
+    $return_error = array();
 
     if (!empty($errors)) {
 
         foreach ($errors as $error) {
 
             $error_message = $error->getElementsByTagName("message")->item(0)->nodeValue;
-            $json = array(
+            $return_error = array(
                 "status" => "error",
                 "message" => $error_message,
                 "data" => array(
@@ -316,12 +391,10 @@ function errorCheck($response) {
                     "request" => $error->getElementsByTagName("object")->item(0)->nodeValue
                 )
             );
-
-            die(json_encode($json));
         }
     }
 
-    return false;
+    return $return_error;
 }
 
 ?>
